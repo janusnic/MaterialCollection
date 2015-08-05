108c76c396d0810663e267b603616aed831074ec
Gabriel Barbosa Nascimento <barbosanascimentogabriel@gmail.com>
2015-08-05 06:20:22 -0300

    Remove bootstrap css lib

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-function clearData(){
+function clearData() {
     localStorage.removeItem('images');
     localStorage.removeItem('name');
     localStorage.removeItem('visit');
@@ -9,13 +9,13 @@ function hasClass(el, cls) {
     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
 }
 
-function switchClasses(elem, class1, class2){
+function switchClasses(elem, class1, class2) {
     elem.classList.remove(class1);
     elem.classList.add(class2);
 }
 
 function fadeOut(elem, speed) {
-    var fadeEffect = setInterval(function () {
+    var fadeEffect = setInterval(function() {
         if (!elem.style.opacity) {
             elem.style.opacity = 1;
         }
@@ -32,7 +32,7 @@ function fadeOut(elem, speed) {
 function fadeIn(elem, speed) {
     elem.style.opacity = 0;
 
-    var fadeEffect = setInterval(function () {
+    var fadeEffect = setInterval(function() {
         if (elem.style.opacity > 0.9) {
             clearInterval(fadeEffect);
         } else {
@@ -77,7 +77,7 @@ function fadeIn(elem, speed) {
 
                 localStorage.removeItem('images');
                 localStorage.setItem('images', JSON.stringify($scope.images));
-            }
+            };
 
             $scope.changeName = function() {
                 var nome = prompt("Set the name of this collection!");
@@ -91,17 +91,17 @@ function fadeIn(elem, speed) {
 
                     localStorage.setItem('name', nome);
                 }
-            }
+            };
 
             $scope.copy = function(item) {
                 window.prompt("URL: ", item.url);
-            }
+            };
 
             $scope.showImage = function(url) {
                 var imageShow = document.getElementsByClassName('image-show')[0];
                 imageShow.style.background = 'url(' + url + ') center / cover';
-                fadeIn(imageShow, 25);
-            }
+                fadeIn(imageShow, 5);
+            };
 
         }]);
 })();
@@ -110,31 +110,45 @@ function fadeIn(elem, speed) {
 var splash = document.getElementsByClassName('splash')[0];
 var splashImage = document.getElementsByClassName('splash-image')[0];
 var nextButton = document.getElementsByClassName('next')[0];
+var prevButton = document.getElementsByClassName('prev')[0];
 var skipButton = document.getElementsByClassName('skip')[0];
 var splashImg1 = document.getElementsByClassName('splash-img')[0];
 var splashImg2 = document.getElementsByClassName('splash-img2')[0];
 //IF ITS NOT THE FIRST TIME, HIDE SPLASH SCREEN
-if(localStorage.getItem('visit')){
+if (localStorage.getItem('visit')) {
     splash.style.display = 'none';
-} else{
+} else {
     splash.style.display = 'block';
 }
 //Skip Button Actions
-skipButton.addEventListener('click', function(){
-    splash.style.display='none';
+skipButton.addEventListener('click', function() {
+    splash.style.display = 'none';
     localStorage.setItem('visit', true);
 }, false);
 //Next Button Actions
-nextButton.addEventListener('click', function(){
+nextButton.addEventListener('click', function() {
     splash.style.background = '#4286F5';
     splashImage.getElementsByTagName('h2')[0].innerHTML = "Automatically saved";
     splashImage.getElementsByTagName('p')[0].innerHTML = "Never lose data, all photos and settings are quickly saved on cache, to your next visit.";
     skipButton.innerHTML = "OK";
     switchClasses(document.getElementsByClassName('dot1')[0], 'dot-active', 'dot-disable');
     switchClasses(document.getElementsByClassName('dot2')[0], 'dot-disable', 'dot-active');
-    fadeOut(nextButton, 25);
+    nextButton.style.display = 'none';
     splashImg1.style.display = 'none';
+    fadeIn(prevButton, 25);
     fadeIn(splashImg2, 30);
+}, false);
+//Prev Button Actions
+prevButton.addEventListener('click', function() {
+    splash.style.background = '#FEA724';
+    splashImage.getElementsByTagName('h2')[0].innerHTML = "Create a collection";
+    splashImage.getElementsByTagName('p')[0].innerHTML = "Name your collection and add images to it, but don't worry, no login required. Quick and simple!";
+    switchClasses(document.getElementsByClassName('dot1')[0], 'dot-disable', 'dot-active');
+    switchClasses(document.getElementsByClassName('dot2')[0], 'dot-active', 'dot-disable');
+    prevButton.style.display = 'none';
+    splashImg2.style.display = 'none';
+    fadeIn(nextButton, 25);
+    fadeIn(splashImg1, 30);
 }, false);
 /* ----- CONTAINER CODE -----*/
 var closeButton = document.getElementById('close');
@@ -145,21 +159,21 @@ var headerAddButton = document.getElementsByClassName('header-add-button')[0];
 var cancelButton = document.getElementById('cancel');
 var addButton = document.getElementById('add');
 //Close Button Action
-closeButton.addEventListener('click', function(){
+closeButton.addEventListener('click', function() {
     fadeOut(imageShow, 30);
     imageShow.style.background = 'black';
 }, false);
 //Fade Input Actions
-fab.addEventListener('click', function(){
+fab.addEventListener('click', function() {
     fadeIn(input, 10);
 }, false);
-headerAddButton.addEventListener('click', function(){
+headerAddButton.addEventListener('click', function() {
     fadeIn(input, 10);
 }, false);
 //Hide Input Actions
-cancelButton.addEventListener('click', function(){
+cancelButton.addEventListener('click', function() {
     input.style.display = 'none';
 }, false);
-addButton.addEventListener('click', function(){
+addButton.addEventListener('click', function() {
     input.style.display = 'none';
 }, false);
