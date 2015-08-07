8b599ff24ff96c1afb16d7217f899678f97abdc7
Gabriel Barbosa Nascimento <barbosanascimentogabriel@gmail.com>
2015-08-07 09:57:28 -0300

    Fixed bug at editing a custom url

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,93 @@
-function clearData() {
-    localStorage.removeItem('images');
-    localStorage.removeItem('name');
-    localStorage.removeItem('visit');
+/**
+ *  Add data to the localStorage cache
+ *  @param {string} A keyname to get some data from the cache
+ *  @param {Object} The data that will be stored
+ */
+function setItem(key, data){
+    localStorage.setItem(key, data);
+}
+
+/**
+ *  Remove data from the localStorage cache
+ *  @param {string} A keyname to get some data from the cache
+ *  @param {Object} The data that will be returned
+ */
+function removeItem(key, data){
+    localStorage.removeItem(key, data);
+}
+
+/**
+ *  Clear all the cache data from the localStorage and reload the page
+ */
+function reset() {
+    removeItem('images');
+    removeItem('name');
+    removeItem('visit');
     location.reload();
 }
 
+/**
+ *  Gets the link of a shared album and splits it as attributes, storing it in a array
+ *  @return {Array<string>} The atributes of the custom Url
+ */
 function getUrlData(){
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&&');
+
     for(var i = 0; i < hashes.length; i++){
         hash = hashes[i].split('==');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
     }
+
     return vars;
 }
 
+/**
+ *  Checks if an element has an specific class
+ *  @param {HTMLElement} The element to check if contains some class
+ *  @param {string} The name of the class to look for in the element
+ *  @return {boolean}
+ */
 function hasClass(el, cls) {
     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
 }
 
-function switchClasses(elem, class1, class2) {
+/**
+ *  Swich one class for another in some element
+ *  @param {HTMLElement} The element desired to swap classes 
+ *  @param {string} The name of the class that will be replaced
+ *  @param {String} The name of the class that will replace the other class
+ */
+function swapClasses(elem, class1, class2) {
     elem.classList.remove(class1);
     elem.classList.add(class2);
 }
 
+/**
+ *  A fadeIn animation effect
+ *  @param {HTMLElement} The element desired 
+ *  @param {int} The speed of the animation
+ */
+function fadeIn(elem, speed) {
+    elem.style.opacity = 0;
+
+    var fadeEffect = setInterval(function() {
+        if (elem.style.opacity > 0.9) {
+            clearInterval(fadeEffect);
+        } else {
+            elem.style.opacity = +elem.style.opacity + 0.1;
+        }
+    }, speed);
+
+    elem.style.display = 'block';
+}
+
+/**
+ *  A fadeOut animation effect
+ *  @param {HTMLElement} The element desired 
+ *  @param {int} The speed of the animation
+ */
 function fadeOut(elem, speed) {
     var fadeEffect = setInterval(function() {
         if (!elem.style.opacity) {
@@ -39,73 +102,84 @@ function fadeOut(elem, speed) {
 
     elem.style.display = 'none';
 }
-
-function fadeIn(elem, speed) {
-    elem.style.opacity = 0;
-
-    var fadeEffect = setInterval(function() {
-        if (elem.style.opacity > 0.9) {
-            clearInterval(fadeEffect);
-        } else {
-            elem.style.opacity = +elem.style.opacity + 0.1;
-        }
-    }, speed);
-
-    elem.style.display = 'block';
-}
 /* ----- ANGULAR CODES ----- */
 (function() {
     angular.module('wallpaper', [])
-        //CONTROLLER
+        // App Controller
         .controller('containerController', ['$scope', function($scope) {
             $scope.name = 'Album';
             $scope.images = [];
 
-            $scope.addItem = function(item) {
+            // Runs the localStorage codes, and gets the return
+            var code = run();
+
+            /**
+             *  Add one image to the array and displays it at the container
+             *  @param {string} The link of the image that will be added 
+             */
+            $scope.addItem = function(imgUrl) {
                 var element = document.getElementById("input-url");
 
                 if (hasClass(element, "is-dirty")) {
                     $scope.images.push({
-                        url: item
+                        url: imgUrl
                     });
 
-                    localStorage.removeItem('images');
-
-                    localStorage.setItem('images', JSON.stringify($scope.images));
+                    // If not editing a shared Url, do cache
+                    if (code[1] === false) {
+                        setItem('images', JSON.stringify($scope.images));
+                    }
                 }
             };
 
+            /**
+             *  Remove one image to the array and from the container
+             *  @param {int} The index of the item that will be removed 
+             */
             $scope.removeItem = function(index) {
                 $scope.images.splice(index, 1);
-
-                localStorage.removeItem('images');
-                localStorage.setItem('images', JSON.stringify($scope.images));
+                // If not editing a shared Url, do cache
+                if (code[1] === false) {
+                    setItem('images', JSON.stringify($scope.images));
+                } 
             };
 
+            /**
+             *  Change the name of the album and save it on the cache
+             */
             $scope.changeName = function() {
                 var nome = prompt("Set the name of this collection!");
 
                 if (nome !== null && nome !== '') {
                     $scope.name = nome;
-
-                    if (localStorage.getItem('name')) {
-                        localStorage.removeItem('name');
-                    }
-
-                    localStorage.setItem('name', nome);
+                    // If not editing a shared Url, do cache
+                    if (code[0] === false) {
+                        setItem('name', nome);
+                    } 
                 }
             };
 
+            /**
+             *  Get the link of an image of the album
+             *  @param {Object} The item (image) from the array
+             */
             $scope.copy = function(item) {
                 window.prompt("URL: ", item.url);
             };
 
+            /**
+             *  Gets the link of the image, and shows the image in a image visualizer  
+             *  @param {string} The url of the image to display on the visualizer
+             */
             $scope.showImage = function(url) {
                 var imageShow = document.getElementsByClassName('image-show')[0];
                 imageShow.style.background = 'url(' + url + ') center / cover';
                 imageShow.style.display = 'block';
             };
 
+            /**
+             *  Transform all the data in a link to share to others
+             */
             $scope.share = function(){
                 var link = "";
                 var array = $scope.images;
@@ -113,64 +187,77 @@ function fadeIn(elem, speed) {
                     link += '!url!' + array[i].url;      
                 }
 
-                if(link == ""){
+                //If it's only sharing the name of the album, with empty photos
+                if (link === "") {
                     link = 'http://gabrielbarbosanascimento.github.io/MaterialCollection/#' + 'name==' + encodeURIComponent($scope.name);   
-                } else{
+                } else {
                     link = 'http://gabrielbarbosanascimento.github.io/MaterialCollection/#' + 'name==' + encodeURIComponent($scope.name) + "&&" + "images==" + link;
                 }
 
                 window.prompt("Share the link bellow: ", link);
-
             };
 
+            /**
+             * Save all the data created to the localStorage cache
+             */
             $scope.save = function(){
-                localStorage.removeItem('images');
-                localStorage.removeItem('name');
-                localStorage.removeItem('visit');
-                localStorage.setItem('images', JSON.stringify($scope.images));
-                localStorage.setItem('name', $scope.name);
-                localStorage.setItem('visit', true);
+                setItem('images', JSON.stringify($scope.images));
+                setItem('name', $scope.name);
+                setItem('visit', true);
                 document.getElementById('clear-data').style.display = 'block';           
                 window.location.assign('http://gabrielbarbosanascimento.github.io/MaterialCollection/');
-            }
+            };
 
-            /* ----- Code for receiving an album link from someone ----- */
-            var nameUrl = getUrlData()['name'];
-            var imagesUrl = getUrlData()['images'];
+            /**
+             *  Function for the dealing on opening links, checking if it's the regular url of if it's a shared url link
+             *  @retun {Array<boolean>} return if the link openend constains an album name or images,
+             *  wherein the first position is for the name and the second for the image
+             */
+            function run(){
+                var code = [false, false];
+                var name = getUrlData().name;
+                var imgs = getUrlData().images;
 
-            //IF GETS AN ALBUMS NAME
-            if(nameUrl === undefined || nameUrl === null){
-                if (localStorage.getItem('name')) {
-                    $scope.name = localStorage.getItem('name');
-                }
-            } else{
-                $scope.name = decodeURIComponent(nameUrl);
-                document.getElementsByClassName('splash')[0].style.display = 'none';
-                document.getElementById('save-data').style.display = 'block';
-                document.getElementById('clear-data').style.display = 'none';    
-            }
-
-            //IF GETS IMAGES
-            if(imagesUrl === undefined || nameUrl === null){
-                if (localStorage.getItem('images')) {
-                    $scope.images = JSON.parse(localStorage.getItem('images'));
-                }
-            } else{ 
-                //Divide the urls
-                var splitUrl = imagesUrl.split('!url!');
-                for (var i = 0; i < splitUrl.length; i++) {
-                    if(i > 0){
-                        $scope.images.push({
-                            url: splitUrl[i]
-                        })
+                // if there's an album's name attr at the link opened
+                if (name === undefined || name === null) {
+                    if (localStorage.getItem('name')) {
+                        $scope.name = localStorage.getItem('name');
                     }
-                };
+                } else{
+                    // Change properties for a shared link page
+                    $scope.name = decodeURIComponent(name);
+                    document.getElementsByClassName('splash')[0].style.display = 'none';
+                    document.getElementById('save-data').style.display = 'block';
+                    document.getElementById('clear-data').style.display = 'none';
+                    code[0] = true;    
+                }
+
+                // if there are imgages attr at the link opened
+                if (imgs === undefined || imgs === null) {
+                    if (localStorage.getItem('images')) {
+                        $scope.images = JSON.parse(localStorage.getItem('images'));
+                    }
+                } else { 
+                    // Divide the link to get the data
+                    var splitUrl = imgs.split('!url!');
+                    for (var i = 0; i < splitUrl.length; i++) {
+                        if(i > 0){
+                            $scope.images.push({
+                                url: splitUrl[i]
+                            });
+                        }
+                    }
+
+                    code[1] = true; 
+                }
+
+                return code;
             }
 
         }]);
 })();
 
-/* ----- SPLASH SCREEN CODES ----- */
+/* ----- SPLASH SCREEN CODE ----- */
 var splash = document.getElementsByClassName('splash')[0];
 var splashImage = document.getElementsByClassName('splash-image')[0];
 var nextButton = document.getElementsByClassName('next')[0];
@@ -178,42 +265,43 @@ var prevButton = document.getElementsByClassName('prev')[0];
 var skipButton = document.getElementsByClassName('skip')[0];
 var splashImg1 = document.getElementsByClassName('splash-img')[0];
 var splashImg2 = document.getElementsByClassName('splash-img2')[0];
-//IF ITS NOT THE FIRST TIME, HIDE SPLASH SCREEN
+// If it's not the first time visiting, hide intro
 if (localStorage.getItem('visit')) {
     splash.style.display = 'none';
 } else {
     splash.style.display = 'block';
 }
-//Skip Button Actions
+
 skipButton.addEventListener('click', function() {
     splash.style.display = 'none';
-    localStorage.setItem('visit', true);
+    setItem('visit', true);
 }, false);
-//Next Button Actions
+
 nextButton.addEventListener('click', function() {
     splash.style.background = '#4286F5';
     splashImage.getElementsByTagName('h2')[0].innerHTML = "Automatically saved";
     splashImage.getElementsByTagName('p')[0].innerHTML = "Never lose data, all photos and settings are quickly saved on cache, to your next visit.";
     skipButton.innerHTML = "OK";
-    switchClasses(document.getElementsByClassName('dot1')[0], 'dot-active', 'dot-disable');
-    switchClasses(document.getElementsByClassName('dot2')[0], 'dot-disable', 'dot-active');
+    swapClasses(document.getElementsByClassName('dot1')[0], 'dot-active', 'dot-disable');
+    swapClasses(document.getElementsByClassName('dot2')[0], 'dot-disable', 'dot-active');
     nextButton.style.display = 'none';
     splashImg1.style.display = 'none';
     fadeIn(prevButton, 25);
     fadeIn(splashImg2, 30);
 }, false);
-//Prev Button Actions
+
 prevButton.addEventListener('click', function() {
     splash.style.background = '#FEA724';
     splashImage.getElementsByTagName('h2')[0].innerHTML = "Create a collection";
     splashImage.getElementsByTagName('p')[0].innerHTML = "Name your collection and add images to it, but don't worry, no login required. Quick and simple!";
-    switchClasses(document.getElementsByClassName('dot1')[0], 'dot-disable', 'dot-active');
-    switchClasses(document.getElementsByClassName('dot2')[0], 'dot-active', 'dot-disable');
+    swapClasses(document.getElementsByClassName('dot1')[0], 'dot-disable', 'dot-active');
+    swapClasses(document.getElementsByClassName('dot2')[0], 'dot-active', 'dot-disable');
     prevButton.style.display = 'none';
     splashImg2.style.display = 'none';
     fadeIn(nextButton, 25);
     fadeIn(splashImg1, 30);
 }, false);
+
 /* ----- CONTAINER CODE -----*/
 var closeButton = document.getElementById('close');
 var imageShow = document.getElementsByClassName('image-show')[0];
@@ -222,22 +310,23 @@ var fab = document.getElementById('fab');
 var headerAddButton = document.getElementsByClassName('header-add-button')[0];
 var cancelButton = document.getElementById('cancel');
 var addButton = document.getElementById('add');
-//Close Button Action
+
 closeButton.addEventListener('click', function() {
     imageShow.style.display = 'none';
     imageShow.style.background = 'black';
 }, false);
-//Fade Input Actions
+
 fab.addEventListener('click', function() {
     fadeIn(input, 10);
 }, false);
 headerAddButton.addEventListener('click', function() {
     fadeIn(input, 10);
 }, false);
-//Hide Input Actions
+
 cancelButton.addEventListener('click', function() {
     input.style.display = 'none';
 }, false);
+
 addButton.addEventListener('click', function() {
     input.style.display = 'none';
 }, false);
